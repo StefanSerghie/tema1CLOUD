7b921420c4333ec2a9a9f3ba85d068cd0937c737
StefanSerghie <81101322+StefanSerghie@users.noreply.github.com>
2021-03-22 19:25:33 +0200

    Add PersonClient

---
+++ tema1CLOUD/PersonClient/PersonClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace PersonClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new PersonManager.PersonManagerClient(channel);
+
+            Console.Write("Name: ");
+            var name = Console.ReadLine();
+            Console.Write("CNP: ");
+            var cnp = Console.ReadLine();
+
+            var personToBeAdded = new Person() { Name = name.Trim().Length > 0 ? name : "undefined" ,
+                Cnp=cnp.Trim().Length>0 ? cnp : "undefined"};
+            var response = await client.AddPersonAsync(
+                              new AddPersonRequest { Person = personToBeAdded });
+            Console.WriteLine("Adding status: " + response.Status);
+        }
+    }
+}
